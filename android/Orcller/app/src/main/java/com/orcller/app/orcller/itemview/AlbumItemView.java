5ffdd1cbf931bf91f0477f014fe2c7bd17357a17
pisces <hh963103@gmail.com>
2015-12-06 21:52:33 +0900

    dev

---
+++ android/Orcller/app/src/main/java/com/orcller/app/orcller/itemview/AlbumItemView.java
@@ -0,0 +1,9 @@
+package com.orcller.app.orcller.itemview;
+
+import pisces.psuikit.ext.PSLinearLayout;
+
+/**
+ * Created by pisces on 12/6/15.
+ */
+public class AlbumItemView extends PSLinearLayout {
+}
